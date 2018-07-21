46e43e96097b4fc98245e9dd9653e7e291c8bec0
Anthony Ang <anthonyang25@gmail.com>
2018-07-21 13:19:52 +0800

    move file to new folder

--- FaceDetectApiSdkDemo/Program.cs
+++ FaceDetectApiSdkDemo/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Microsoft.ProjectOxford.Face;
+using System;
+using System.Threading.Tasks;
 
 namespace FaceDetectApiSdkDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IFaceServiceClient faceServiceClient = CreateFaceClient();
+
+            var faceAttri = new[] { FaceAttributeType.Emotion, FaceAttributeType.Age };
+            var faces = await faceServiceClient.DetectAsync("https://psfaceapicourse.blob.core.windows.net/images/diverse-people.png",
+                returnFaceAttributes : faceAttri);
+
+            foreach (var face in faces)
+            {
+                Console.WriteLine("-------Face-------");
+                Console.WriteLine($"{face.FaceId}");
+                Console.WriteLine($"T: {face.FaceRectangle.Top}; L: {face.FaceRectangle.Left}; " +
+                    $"W: {face.FaceRectangle.Width}; H: {face.FaceRectangle.Height}");
+                Console.WriteLine($"Age: {face.FaceAttributes.Age}; Happiness: {face.FaceAttributes.Emotion.Happiness}");
+            }
         }
+
+        static IFaceServiceClient CreateFaceClient() => new FaceServiceClient("<SubscriptionKey>", "https://eastasia.api.cognitive.microsoft.com/face/v1.0");
+        
+
     }
 }
